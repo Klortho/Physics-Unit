e8e58fb4147323e36df81b588e1d91c1a6d6b7e3
Joel Berger <joel.a.berger@gmail.com>
2012-04-19 11:48:22 -0500

    use heredoc for subclass template

--- lib/Physics/Unit/Scalar.pm
+++ lib/Physics/Unit/Scalar.pm
@@ -9,7 +9,7 @@ $VERSION = eval $VERSION;
 use Physics::Unit ':ALL';
 
 # This is the actual content of a user defined unit.
-my $subclass_template = q(
+my $subclass_template = <<'END_TEMPLATE';
 package Physics::Unit::%s;
 use strict;
 use base qw(Physics::Unit::Scalar);
@@ -18,7 +18,7 @@ use vars qw($MyUnit $DefaultUnit);
 $MyUnit = GetUnit('%s');
 $DefaultUnit = $MyUnit;
 1;
-);
+END_TEMPLATE
 
 # Call the InitSubtypes() function.
 InitSubtypes();
