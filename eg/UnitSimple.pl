fa0377758ac65507251dfda5eee37b7b0d3a7521
Chris Maloney <voldrani@gmail.com>
2012-04-29 18:33:10 -0400

    Removing some unneeded example files

--- eg/UnitSimple.pl
+++
@@ -1,9 +0,0 @@
-use Physics::Unit ':ALL';
-
-
-$mile = GetUnit('mile');
-$foot = GetUnit('foot');
-$c = $mile->convert($foot);     # $c == 5280
-
-print "A mile is $c feet\n";
-
--- eg/VectorSimple.pl
+++
@@ -1,7 +0,0 @@
-BEGIN {
-    $Physics::Unit::Vector::debug = 1;
-}
-
-use Physics::Unit::Vector;
-
-$v = new Physics::Unit::Vector('3m', '4m', '5m');
