b705c7fbda9d23800fd70ed8f2f25c1dd03f647e
Chris Maloney <voldrani@gmail.com>
2012-05-12 18:20:54 -0400

    #18 Updates to documentation, UnitDocExamples.pl

--- eg/UnitDocExamples.pl
+++ eg/UnitDocExamples.pl
@@ -2,107 +2,126 @@
 # documentation.
 
 use Physics::Unit ':ALL';   # exports all util. function names
+use Data::Dumper;
 
-# Define your own unit named "ff"
-$ff = new Physics::Unit('furlong / fortnight', 'ff');
-print $ff->type, "\n";         # prints:  Speed
+my %example_num;
+sub header {
+    my ($doc, $sec) = @_;
+    my $num = $example_num{$doc . $sec}++;
 
-# Convert to mph; this prints:  One ff is 0.0003720... miles per hour
-print "One ", $ff->name, " is ", $ff->convert('mph'), " miles per hour\n";
+    print "-" x 70, "\n";
+    print "$doc - $sec, $num\n\n";
+}
 
-# Get canonical string representation
-print $ff->expanded, "\n";     # prints:  0.0001663... m s^-1
+#-----------------------------------------------------------
+header("Physics::Unit", "SYNOPSIS");
 
-# More intricate unit expression (using the newly defined unit 'ff'):
-$gonzo = new Physics::Unit "13 square millimeters per ff";
-print $gonzo->expanded, "\n";  # prints:  0.07816... m s
-
-# Doing arithmetic maintains the types of units
-$m = $ff->copy->times("5 kg");
-print "This ", $m->type, " unit is ", $m->ToString, "\n";
-# prints: This Momentum unit is 0.8315... m gm s^-1
-
-
-
-
-#---------------------
-
-print "One mph is ", GetUnit('mph')->factor, " meters / sec\n";
-
-#---------------------
-
-InitBaseUnit('Beauty' => ['sonja', 'sonjas', 'yh']);
-
-#---------------------
-
-InitPrefix('gonzo' => 1e100, 'piccolo' => 1e-100);
-
-$beauty_rate = new Physics::Unit('5 piccolosonjas / hour');
-
-#---------------------
-
-InitUnit( ['chris', 'cfm'] => '3 piccolosonjas' );
-
-#---------------------
-
-InitTypes( 'Aging' => 'chris / year' );
-
-#---------------------
-
-# Create a new, anonymous unit:
-$u = new Physics::Unit ('3 pi sonjas per s');
-
-# Create a new, named unit:
-$u = new Physics::Unit ('3 pi sonjas per s', 'bloom');
-
-# Create a new unit with a list of names:
-$u  = new Physics::Unit ('3 pi sonjas per s', 'b', 'blooms', 'blm');
-$n = $u->name;    # returns 'b'
-
-#---------------------
-
-$u1 = new Physics::Unit('kg m^2/s^2');
-$t = $u1->type;       #  $t == ['Energy', 'Torque']
-
-$u1->type('Energy');  #  This establishes the type once and for all
-$t = $u1->type;       #  $t == 'Energy'
+    use Physics::Unit ':ALL';   # exports all util. function names
+    # Define your own unit named "ff"
+    $ff = new Physics::Unit('furlong / fortnight', 'ff');
+    print $ff->type, "\n";         # prints:  Speed
+    # Convert to mph; this prints:  One ff is 0.0003720... miles per hour
+    print "One ", $ff->name, " is ", $ff->convert('mph'), " miles per hour\n";
+    # Get canonical string representation
+    print $ff->expanded, "\n";     # prints:  0.0001663... m s^-1
+    # More intricate unit expression (using the newly defined unit 'ff'):
+    $gonzo = new Physics::Unit "13 square millimeters per ff";
+    print $gonzo->expanded, "\n";  # prints:  0.07816... m s
+    # Doing arithmetic maintains the types of units
+    $m = $ff->copy->times("5 kg");
+    print "This ", $m->type, " unit is ", $m->ToString, "\n";
+    # prints: This Momentum unit is 0.8315... m gm s^-1
 
 
-# Now create another Unit object from the same definition
-$u2 = new Physics::Unit('kg m^2/s^2');
+#-----------------------------------------------------------
+header("Physics::Unit", "Functions");
 
-# This is a brand-new object, so the explicit type is unknown, as before:
-$t = $u2->type;    # $t == ['Energy', 'Torque']
+  InitBaseUnit('Beauty' => ['sonja', 'sonjas', 'yh']);
+
+  print "Beauty:  " . Physics::Unit::LookName('Beauty') . "\n";
+
+#-----------------------------------------------------------
+header("Physics::Unit", "Functions");
+
+  InitPrefix('gonzo' => 1e100, 'piccolo' => 1e-100);
+  $beautification_rate = new Physics::Unit('5 piccolosonjas / hour');
+
+  print "gonzo:  " . Physics::Unit::LookName('gonzo') . "\n";
+  print "piccolo:  " . Physics::Unit::LookName('piccolo') . "\n";
+  print "beautification_rate:  " . $beautification_rate->ToString() . "\n";
+
+#-----------------------------------------------------------
+header("Physics::Unit", "Functions");
+
+  InitUnit( ['chris', 'cfm'] => '3 piccolosonjas' );
+
+  print "chris:  " . GetUnit('chris')->ToString() . "\n";
+  print "cfm:  " . GetUnit('cfm')->ToString() . "\n";
+
+#-----------------------------------------------------------
+header("Physics::Unit", "Functions");
+
+  InitUnit( ['mycron'], '3600 sec' );
+  print Dumper(GetUnit('mycron'));
+
+  InitUnit( ['mycron'], 'hour' );
+  InitUnit( ['mycron'], GetUnit('hour') );
+
+#-----------------------------------------------------------
+header("Physics::Unit", "Functions");
+
+  InitTypes( 'Blooming' => 'sonja / year' );
+
+  print "Blooming:  " . Physics::Unit::LookName('gonzo') . "\n";
+
+#-----------------------------------------------------------
+header("Physics::Unit", "Methods");
+
+  # Create a new, named unit:
+  $u = new Physics::Unit ('3 pi furlongs', 'gorkon');
+
+  print Dumper($u);
+
+#-----------------------------------------------------------
+header("Physics::Unit", "Methods");
+
+  print GetUnit('rod')->type, "\n";  # 'Distance'
+
+#-----------------------------------------------------------
+header("Physics::Unit", "Methods");
+
+  $u1 = new Physics::Unit('kg m^2/s^2');
+  $t = $u1->type;       #  ['Energy', 'Torque']
+  print Dumper $t;
+  $u1->type('Energy');  #  This establishes the type once and for all
+  $t = $u1->type;       #  'Energy'
+  print Dumper $t;
+  # Create a new copy of a predefined, typed unit:
+  $u3 = GetUnit('joule')->new;
+  $t = $u3->type;       # 'Energy'
+  print Dumper $t;
+
+#-----------------------------------------------------------
+header("Physics::Unit", "Methods");
+
+  print GetUnit('calorie')->expanded, "\n";  # "4184 m^2 gm s^-2"
+
+#-----------------------------------------------------------
+header("Physics::Unit", "Methods");
+
+  print GetUnit('mile')->convert('foot'), "\n";  # 5280
+
+#-----------------------------------------------------------
+header("Physics::Unit", "Methods");
+
+  $u = new Physics::Unit('36 m^2');
+  print $u->ToString() . "\n";
+  $u->divide('3 meters');   # now '12 m'
+  print $u->ToString() . "\n";
+  $u->divide(3);            # now '4 m'
+  print $u->ToString() . "\n";
+  $u->divide('.5 sec');     # now '8 m/s'
+  print $u->ToString() . "\n";
 
 
-# Create a new copy of a predefined, typed unit:
-$u3 = GetUnit('joule')->new;
-$t = $u3->type;       # 'Energy'
-print "u3->type is $t\n";
-
-# But take care; if you use the C<new()> method with a name, then that's
-# considered a unit expression, and the type is not preserved
-$u4 = new Physics::Unit('joule');
-$t = $u4->type;       # ['Energy', 'Torque']
-print "u4->type is $t\n";
-
-#---------------------
-
-print GetUnit('calorie')->expanded, "\n";
-
-#---------------------
-
-$mile = GetUnit('mile');
-$foot = GetUnit('foot');
-$c = $mile->convert($foot);     # returns 5280
-
-#---------------------
-
-$u = new Physics::Unit('36 m^2');
-$u->divide('3 meters');    # $u is now '12 m'
-$u->divide(3);             # $u is now '4 m'
-$u->divide( new Physics::Unit('.5 sec') );  # $u is now '8 m/s'
-
-#---------------------
-
-print "ok\n";
+print "\n\nok\n";
